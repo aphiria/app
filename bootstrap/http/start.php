c7b8ca1f0311442adede7d99b138743a0d0251ad
opulencephp <dbyoung2@gmail.com>
2019-01-26 19:16:00 -0600

    Moved some bootstrapper dispatcher/factory into config directory

--- bootstrap/http/start.php
+++ bootstrap/http/start.php
@@ -9,11 +9,6 @@
  */
 
 use Opulence\Api\ApiKernel;
-use Opulence\Environments\Environment;
-use Opulence\Ioc\Bootstrappers\Caching\FileCache;
-use Opulence\Ioc\Bootstrappers\Factories\{BootstrapperRegistryFactory, CachedBootstrapperRegistryFactory};
-use Opulence\Ioc\Bootstrappers\IBootstrapperRegistry;
-use Opulence\Ioc\Bootstrappers\Dispatchers\{BootstrapperDispatcher, IBootstrapperDispatcher};
 use Opulence\Net\Http\ContentNegotiation\NegotiatedResponseFactory;
 use Opulence\Net\Http\{RequestFactory, ResponseWriter};
 
@@ -27,31 +22,7 @@ $negotiatedResponseFactory = new NegotiatedResponseFactory($contentNegotiator);
 $exceptionHandler = require "{$paths['config.framework.http']}/exception_handler.php";
 $routeMatcher = require "{$paths['config.framework.http']}/route_matcher.php";
 $dependencyResolver = require "{$paths['config.framework.http']}/dependency_resolver.php";
-
-/**
- * ----------------------------------------------------------
- * Configure the bootstrappers for the HTTP kernel
- * ----------------------------------------------------------
- */
-$httpBootstrapperPath = "{$paths['config.http']}/bootstrappers.php";
-$httpBootstrappers = require $httpBootstrapperPath;
-$allBootstrappers = array_merge($globalBootstrappers, $httpBootstrappers);
-
-// If you should cache your bootstrapper registry
-if (Environment::getVar('ENV_NAME') === Environment::PRODUCTION) {
-    $bootstrapperCache = new FileCache(
-        "{$paths['tmp.framework.http']}/cachedBootstrapperRegistry.json"
-    );
-    $bootstrapperFactory = new CachedBootstrapperRegistryFactory($bootstrapperResolver, $bootstrapperCache);
-    $bootstrapperRegistry = $bootstrapperFactory->createBootstrapperRegistry($allBootstrappers);
-} else {
-    $bootstrapperFactory = new BootstrapperRegistryFactory($bootstrapperResolver);
-    $bootstrapperRegistry = $bootstrapperFactory->createBootstrapperRegistry($allBootstrappers);
-}
-
-$bootstrapperDispatcher = new BootstrapperDispatcher($container, $bootstrapperRegistry, $bootstrapperResolver);
-$container->bindInstance(IBootstrapperRegistry::class, $bootstrapperRegistry);
-$container->bindInstance(IBootstrapperDispatcher::class, $bootstrapperDispatcher);
+require "{$paths['config.framework.http']}/bootstrappers.php";
 
 /**
  * ----------------------------------------------------------
