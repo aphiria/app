27a8b1d618d3fde2ff4ee1bccc3b3035533e0df2
davidbyoung <dbyoung2@gmail.com>
2019-04-01 07:18:01 -0500

    Removed duplicate file header

--- localhost_router.php
+++ localhost_router.php
@@ -9,13 +9,7 @@
  */
 
 declare(strict_types=1);
-/*
- * Aphiria
- *
- * @link      https://www.aphiria.com
- * @copyright Copyright (C) 2019 David Young
- * @license   https://github.com/aphiria/app/blob/master/LICENSE.md
- */
+
 $requestUri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 if ($requestUri !== '/' && file_exists(__DIR__ . "/public$requestUri")) {
