161656c2ae560bb5fcb8089c1a45899528ac34f7
davidbyoung <dbyoung2@gmail.com>
2020-01-16 16:56:21 -0600

    Added problem details to default exception response factory

--- src/Api/Bootstrappers/ExceptionHandlerBootstrapper.php
+++ src/Api/Bootstrappers/ExceptionHandlerBootstrapper.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 
 namespace App\Api\Bootstrappers;
 
+use Aphiria\Api\Errors\ProblemDetails;
 use Aphiria\Api\Errors\ProblemDetailsResponseMutator;
 use Aphiria\Api\Validation\InvalidRequestBodyException;
 use Aphiria\Api\Validation\ValidationProblemDetails;
@@ -24,11 +25,16 @@ use Aphiria\Exceptions\ExceptionResponseFactoryRegistry;
 use Aphiria\Exceptions\GlobalExceptionHandler;
 use Aphiria\Exceptions\IExceptionLogger;
 use Aphiria\Exceptions\IExceptionResponseFactory;
+use Aphiria\IO\Streams\Stream;
 use Aphiria\Net\Http\ContentNegotiation\INegotiatedResponseFactory;
+use Aphiria\Net\Http\ContentNegotiation\MediaTypeFormatters\JsonMediaTypeFormatter;
 use Aphiria\Net\Http\HttpException;
 use Aphiria\Net\Http\HttpStatusCodes;
 use Aphiria\Net\Http\IHttpRequestMessage;
+use Aphiria\Net\Http\Response;
+use Aphiria\Net\Http\StreamBody;
 use Aphiria\Net\Http\StreamResponseWriter;
+use Exception;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 
@@ -50,7 +56,41 @@ final class ExceptionHandlerBootstrapper extends Bootstrapper
          * Register exception response factories for common exceptions
          */
         $exceptionResponseFactoryRegistry = new ExceptionResponseFactoryRegistry();
-        $container->bindInstance(ExceptionResponseFactoryRegistry::class, $exceptionResponseFactoryRegistry);
+
+        // This is the default response factory for exceptions that do not have custom responses
+        $exceptionResponseFactoryRegistry->registerDefaultFactory(
+            function (Exception $ex, ?IHttpRequestMessage $request, INegotiatedResponseFactory $responseFactory) use ($container) {
+                $problemDetails = new ProblemDetails(
+                    'https://tools.ietf.org/html/rfc7231#section-6.6.1',
+                    'An error occurred',
+                    null,
+                    HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR
+                );
+
+                if ($request === null) {
+                    /**
+                     * If the request hasn't even been initialized, the exception must've happened very early in the
+                     * application pipeline.  So, just default to a JSON response.
+                     */
+                    $response = new Response(HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR);
+                    $response->getHeaders()->add('Content-Type', 'application/problem+json');
+                    $bodyStream = new Stream(fopen('php://temp', 'r+b'));
+                    /** @var JsonMediaTypeFormatter $mediaTypeFormatter */
+                    $mediaTypeFormatter = $container->resolve(JsonMediaTypeFormatter::class);
+                    $mediaTypeFormatter->writeToStream($problemDetails, $bodyStream, null);
+                    $response->setBody(new StreamBody($bodyStream));
+
+                    return $response;
+                }
+
+                // Since we have a request, let's use content negotiation to create the response
+                $response = $responseFactory->createResponse($request, HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR, null, $problemDetails);
+
+                return (new ProblemDetailsResponseMutator)->mutateResponse($response);
+            }
+        );
+
+        // These are the response factories for exceptions that have custom responses
         $exceptionResponseFactoryRegistry->registerManyFactories([
             HttpException::class => fn (HttpException $ex, IHttpRequestMessage $request) => $ex->getResponse(),
             InvalidRequestBodyException::class => function (InvalidRequestBodyException $ex, IHttpRequestMessage $request, INegotiatedResponseFactory $responseFactory) {
@@ -64,6 +104,8 @@ final class ExceptionHandlerBootstrapper extends Bootstrapper
             $container->resolve(INegotiatedResponseFactory::class),
             $exceptionResponseFactoryRegistry
         );
+
+        $container->bindInstance(ExceptionResponseFactoryRegistry::class, $exceptionResponseFactoryRegistry);
         $container->bindInstance(IExceptionResponseFactory::class, $exceptionResponseFactory);
 
         /**
@@ -80,7 +122,7 @@ final class ExceptionHandlerBootstrapper extends Bootstrapper
                     return LogLevel::CRITICAL;
                 }
 
-                return LogLevel::ERROR;
+                return LogLevel::INFO;
             }
         ]);
 
