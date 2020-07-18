c272e394a673ef7ae05f8386e56fbfaec42dc65f
davidbyoung <dbyoung2@gmail.com>
2020-07-18 15:54:14 -0500

    Updated to use 0.x branch of Aphiria, removed file headers to clear up confusion as to who owns what

--- src/Demo/UserModule.php
+++ src/Demo/UserModule.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * Aphiria
- *
- * @link      https://www.aphiria.com
- * @copyright Copyright (C) 2020 David Young
- * @license   https://github.com/aphiria/app/blob/master/LICENSE.md
- */
-
 declare(strict_types=1);
 
 namespace App\Demo;
