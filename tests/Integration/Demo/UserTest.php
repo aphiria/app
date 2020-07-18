5deea30fd2633d1b23273701aa9e1d9bd63a6fe1
davidbyoung <dbyoung2@gmail.com>
2020-07-18 14:46:09 -0500

    Changed integration test so that it only seeds users when they're needed, updated the integration test to seed multiple users

--- tests/Integration/Demo/UserTest.php
+++ tests/Integration/Demo/UserTest.php
@@ -17,27 +17,24 @@ use App\Tests\Integration\IntegrationTestCase;
 
 class UserTest extends IntegrationTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // Seed a user
-        $this->post('/users', [], new User(123, 'foo@bar.com'));
-    }
-
     protected function tearDown(): void
     {
         parent::tearDown();
 
+        // Clean up after our integration tests
         $this->delete('/users');
     }
 
     public function testGettingAllUsers(): void
     {
+        // Seed some users
+        $this->post('/users', [], new User(123, 'foo@bar.com'));
+        $this->post('/users', [], new User(456, 'baz@qux.com'));
+        // Get the users
         $response = $this->get('/users?letMeIn=1');
         $this->assertStatusCodeEquals(200, $response);
         $this->assertParsedBodyEquals(
-            [new User(123, 'foo@bar.com')],
+            [new User(123, 'foo@bar.com'), new User(456, 'baz@qux.com')],
             $response
         );
     }
