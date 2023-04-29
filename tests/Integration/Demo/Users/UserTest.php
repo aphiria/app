60073a848cc580d987aa8752efebb38e4ef523d1
David Young <dbyoung2@gmail.com>
2023-04-29 17:43:08 -0500

    Started implementing some integration tests

--- tests/Integration/Demo/Users/UserTest.php
+++ tests/Integration/Demo/Users/UserTest.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace App\Tests\Integration\Demo\Users;
 
+use Aphiria\Authentication\AuthenticationResult;
+use Aphiria\Authentication\IAuthenticator;
+use Aphiria\Authentication\IUserAccessor;
 use Aphiria\DependencyInjection\Container;
 use Aphiria\Net\Http\HttpStatusCode;
+use Aphiria\Net\Http\IRequest;
+use Aphiria\Net\Http\IResponse;
+use Aphiria\Security\Claim;
+use Aphiria\Security\ClaimType;
+use Aphiria\Security\Identity;
+use Aphiria\Security\IPrincipal;
+use Aphiria\Security\User;
 use App\Demo\Users\NewUser;
 use App\Demo\Users\UserSeeder;
 use App\Demo\Users\UserViewModel;
@@ -16,6 +26,7 @@ class UserTest extends IntegrationTestCase
 {
     /** @var list<UserViewModel> The list of users to delete at the end of each test */
     private array $createdUsers = [];
+    private IAuthenticator $authenticator;
 
     protected function setUp(): void
     {
@@ -23,6 +34,15 @@ class UserTest extends IntegrationTestCase
 
         $userSeeder = Container::$globalInstance?->resolve(UserSeeder::class);
         $userSeeder?->seed();
+        $this->createTestingAuthenticator();
+
+        /**
+         * TODO
+         *
+         * Should also create mockable IAuthority
+         *      This should probably do some real work, eg looking up policy by name like the real one so that integration tests are slightly more realistic.  This would require some refactoring to either an abstract class or an overridable Authority class.
+         * Should update mockable IAuthenticator to likewise to some more realistic lookups to ensure that things like schemes actually exist
+         */
     }
 
     protected function tearDown(): void
@@ -36,29 +56,50 @@ class UserTest extends IntegrationTestCase
         }
     }
 
-    #[TestWith([[new NewUser('foo@bar.com', 'foo')]])]
-    public function testCreatingUsersMakesThemRetrievable(NewUser $newUser): void
+    #[TestWith([new NewUser('foo@bar.com', 'foo')])]
+    public function testCreatingUsersMakesThemRetrievableAsAdminUser(NewUser $newUser): void
     {
         // Create some users
-        // $this->createdUsers[] = ??? TODO: Need to somehow populate createdUsers with "UserViewModel" objects
-        $this->post('/demo/users', [], $newUser);
+        $response = $this->post('/demo/users', [], $newUser);
+        /** @var UserViewModel $createdUser */
+        $createdUser = $this->negotiateResponseBody(UserViewModel::class, $response);
+        // Make sure we clean this user up later
+        $this->createdUsers[] = $createdUser;
 
         // Check that the user can be retrieved
-        $response = $this->get("/demo/users/$user->id");
+        $claims = [
+            new Claim(ClaimType::NameIdentifier, 'foo', 'example.com'),
+            new Claim(ClaimType::Role, 'admin', 'example.com')
+        ];
+        $adminUser = new User(new Identity($claims));
+        $response = $this->actingAs($adminUser)->get("/demo/users/{$createdUser->id}");
         $this->assertStatusCodeEquals(200, $response);
-        $this->assertParsedBodyEquals($user, $response);
+        $this->assertParsedBodyEquals($createdUser, $response);
     }
 
     public function testDeletingAnotherUserAsAdminReturns204(): void
     {
-        // TODO
-        $response = $this->actingAs($someUser)->delete('/demo/users/someId');
-        $this->assertStatusCodeEquals(HttpStatusCode::NoContent, $response);
+        $claims = [
+            new Claim(ClaimType::NameIdentifier, 'foo', 'example.com'),
+            new Claim(ClaimType::Role, 'admin', 'example.com')
+        ];
+        $adminUser = new User(new Identity($claims));
+        $createUserResponse = $this->post('/demo/users', body: new NewUser('test@example.com', 'password'));
+        /** @var UserViewModel $createdUser */
+        $createdUser = $this->negotiateResponseBody(UserViewModel::class, $createUserResponse);
+        $deleteUserResponse = $this->actingAs($adminUser)->delete("/demo/users/$createdUser->id");
+        $this->assertStatusCodeEquals(HttpStatusCode::NoContent, $deleteUserResponse);
     }
 
     public function testDeletingAnotherUserAsNonAdminReturns403(): void
     {
-        // TODO
+        $claims = [
+            new Claim(ClaimType::NameIdentifier, 'foo', 'example.com')
+        ];
+        $nonAdminUser = new User(new Identity($claims));
+        // TODO: This is a hard-coded user ID.  Needs to be dynamically created once I've figured out way of grabbing parsed body.
+        $response = $this->actingAs($nonAdminUser)->delete('/demo/users/452b0ee6-7597-45a1-83e2-c70f2ad939f0');
+        $this->assertStatusCodeEquals(HttpStatusCode::Forbidden, $response);
     }
 
     public function testDeletingNonExistentUserReturns404(): void
@@ -85,5 +126,67 @@ class UserTest extends IntegrationTestCase
     public function testGettingPagedUsersReturnsSuccessfullyForAdmins(): void
     {
         // TODO
+    }
+
+    // TODO: Remove this once I've done some PoC work
+    protected function createTestingAuthenticator(): void
+    {
+        $this->authenticator = new class (Container::$globalInstance?->resolve(IUserAccessor::class)) implements IAuthenticator {
+            public ?AuthenticationResult $expectedAuthenticationResult = null;
+
+            public function __construct(private readonly IUserAccessor $userAccessor)
+            {
+            }
+
+            public function authenticate(IRequest $request, string $schemeName = null): AuthenticationResult
+            {
+                if ($this->expectedAuthenticationResult === null) {
+                    throw new \Exception('Expected authentication result is not set');
+                }
+
+                if ($this->expectedAuthenticationResult->passed) {
+                    $this->userAccessor->setUser($this->expectedAuthenticationResult->user, $request);
+                }
+
+                return $this->expectedAuthenticationResult;
+            }
+
+            public function challenge(IRequest $request, IResponse $response, string $schemeName = null): void
+            {
+                // TODO: Implement challenge() method.
+            }
+
+            public function forbid(IRequest $request, IResponse $response, string $schemeName = null): void
+            {
+                // TODO: Implement forbid() method.
+            }
+
+            public function logIn(
+                IPrincipal $user,
+                IRequest $request,
+                IResponse $response,
+                string $schemeName = null
+            ): void {
+                $this->userAccessor->setUser($user, $request);
+            }
+
+            public function logOut(IRequest $request, IResponse $response, string $schemeName = null): void
+            {
+                $this->userAccessor->setUser(null, $request);
+            }
+        };
+
+        // Bind these mocks to the container
+        Container::$globalInstance?->bindInstance(IAuthenticator::class, $this->authenticator);
+    }
+
+    // TODO: Figure out where to actually put this logic
+    // TODO: This should be scoped for a single request, whereas it's setting the user for all subsequent requests.  Maybe it should take a callback with the client call or something?
+    protected function actingAs(IPrincipal $user): static
+    {
+        // TODO: How should this work in real life?  I'd have to check if IAuthenticator is an instance of some new TestAuthentication, which would have that public property, and if so it'd set that property.  However, is that too constraining to impose a specific test authenticator on users?
+        $this->authenticator->expectedAuthenticationResult = AuthenticationResult::pass($user);
+
+        return $this;
     }
 }
