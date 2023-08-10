c6fb36b0819e173040ff72428a631d42d5057ab1
davidbyoung <dbyoung2@gmail.com>
2023-08-10 15:11:35 -0500

    Code style

--- tests/Integration/CreatesUser.php
+++ tests/Integration/CreatesUser.php
@@ -30,10 +30,11 @@ trait CreatesUser
         // Create a unique email address so we do not have collisions
         $roles = \is_string($roles) ? [$roles] : $roles;
         $newUser = new NewUser(\bin2hex(\random_bytes(8)) . '@example.com', $password, $roles);
+        $principalBuilder = (new PrincipalBuilder('example.com'))->withNameIdentifier(0);
         $actingAs = $createAsAdmin
-            ? (new PrincipalBuilder('example.com'))->withRoles('admin')
+            ? $principalBuilder->withRoles('admin')
                 ->build()
-            : (new PrincipalBuilder('example.com'))->build();
+            : $principalBuilder->build();
         $createUserResponse = $this->actingAs($actingAs, fn () => $this->post('/users', body: $newUser));
 
         if ($createUserResponse->getStatusCode() !== HttpStatusCode::Ok) {
