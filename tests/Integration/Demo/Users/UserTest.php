41ea2a78498958b5fcb0d3a35105631edb7ec641
David Young <dbyoung2@gmail.com>
2023-08-18 16:03:41 -0500

    Updated to use simpler PrincipalBuilder

--- tests/Integration/Demo/Users/UserTest.php
+++ tests/Integration/Demo/Users/UserTest.php
@@ -7,7 +7,6 @@ namespace App\Tests\Integration\Demo\Users;
 use Aphiria\DependencyInjection\Container;
 use Aphiria\Net\Http\HttpStatusCode;
 use Aphiria\Security\Identity;
-use Aphiria\Security\IdentityBuilder;
 use Aphiria\Security\PrincipalBuilder;
 use Aphiria\Security\User as Principal;
 use App\Tests\Integration\Demo\Authenticates;
@@ -40,11 +39,9 @@ class UserTest extends IntegrationTestCase
         $createdUser = $this->createUser();
 
         // Check that the user can be retrieved
-        $adminUser = (new PrincipalBuilder('example.com'))
-            ->withIdentity(function (IdentityBuilder $identity) {
-                $identity->withNameIdentifier(1)
-                    ->withRoles('admin');
-            })->build();
+        $adminUser = (new PrincipalBuilder('example.com'))->withNameIdentifier(1)
+            ->withRoles('admin')
+            ->build();
         $response = $this->actingAs($adminUser, fn () => $this->get("/demo/users/$createdUser->id"));
         $this->assertStatusCodeEquals(HttpStatusCode::Ok, $response);
         $this->assertParsedBodyEquals($createdUser, $response);
@@ -55,11 +52,9 @@ class UserTest extends IntegrationTestCase
         $createdUser = $this->createUser();
 
         // Try deleting the created user
-        $adminUser = (new PrincipalBuilder('example.com'))
-            ->withIdentity(function (IdentityBuilder $identity) {
-                $identity->withNameIdentifier(1)
-                    ->withRoles('admin');
-            })->build();
+        $adminUser = (new PrincipalBuilder('example.com'))->withNameIdentifier(1)
+            ->withRoles('admin')
+            ->build();
         $response = $this->actingAs($adminUser, fn () => $this->delete("/demo/users/$createdUser->id"));
         $this->assertStatusCodeEquals(HttpStatusCode::NoContent, $response);
     }
@@ -69,21 +64,17 @@ class UserTest extends IntegrationTestCase
         $createdUser = $this->createUser();
 
         // Try deleting the created user
-        $nonAdminUser = (new PrincipalBuilder('example.com'))
-            ->withIdentity(function (IdentityBuilder $identity) {
-                $identity->withNameIdentifier(1);
-            })->build();
+        $nonAdminUser = (new PrincipalBuilder('example.com'))->withNameIdentifier(1)
+            ->build();
         $response = $this->actingAs($nonAdminUser, fn () => $this->delete("/demo/users/$createdUser->id"));
         $this->assertStatusCodeEquals(HttpStatusCode::Forbidden, $response);
     }
 
     public function testDeletingNonExistentUserReturns404(): void
     {
-        $adminUser = (new PrincipalBuilder('example.com'))
-            ->withIdentity(function (IdentityBuilder $identity) {
-                $identity->withNameIdentifier(1)
-                    ->withRoles('admin');
-            })->build();
+        $adminUser = (new PrincipalBuilder('example.com'))->withNameIdentifier(1)
+            ->withRoles('admin')
+            ->build();
         // Pass in a dummy user ID
         $response = $this->actingAs($adminUser, fn () => $this->get('/demo/users/0'));
         $this->assertStatusCodeEquals(HttpStatusCode::NotFound, $response);
@@ -94,10 +85,8 @@ class UserTest extends IntegrationTestCase
         $createdUser = $this->createUser();
 
         // Try deleting the created user
-        $createdUserPrincipal = (new PrincipalBuilder('example.com'))
-            ->withIdentity(function (IdentityBuilder $identity) use ($createdUser) {
-                $identity->withNameIdentifier($createdUser->id);
-            })->build();
+        $createdUserPrincipal = (new PrincipalBuilder('example.com'))->withNameIdentifier($createdUser->id)
+            ->build();
         $response = $this->actingAs($createdUserPrincipal, fn () => $this->delete("/demo/users/$createdUser->id"));
         $this->assertStatusCodeEquals(HttpStatusCode::NoContent, $response);
     }
@@ -111,10 +100,8 @@ class UserTest extends IntegrationTestCase
 
     public function testGettingPagedUsersRedirectsToForbiddenPageForNonAdmins(): void
     {
-        $nonAdminUser = (new PrincipalBuilder('example.com'))
-            ->withIdentity(function (IdentityBuilder $identity) {
-                $identity->withNameIdentifier(1);
-            })->build();
+        $nonAdminUser = (new PrincipalBuilder('example.com'))->withNameIdentifier(1)
+            ->build();
         $response = $this->actingAs($nonAdminUser, fn () => $this->get('/demo/users'));
         $this->assertStatusCodeEquals(HttpStatusCode::Found, $response);
         $this->assertHeaderEquals('/access-denied', $response, 'Location');
@@ -125,11 +112,9 @@ class UserTest extends IntegrationTestCase
         // Since our testing database may have many users, we can't know for sure what the result will be
         // So, just test that this returns ok
         $this->createUser();
-        $adminUser = (new PrincipalBuilder('example.com'))
-            ->withIdentity(function (IdentityBuilder $identity) {
-                $identity->withNameIdentifier(1)
-                    ->withRoles('admin');
-            })->build();
+        $adminUser = (new PrincipalBuilder('example.com'))->withNameIdentifier(1)
+            ->withRoles('admin')
+            ->build();
         $response = $this->actingAs($adminUser, fn () => $this->get('/demo/users'));
         $this->assertStatusCodeEquals(HttpStatusCode::Ok, $response);
         $this->assertNotEmpty($response->getBody()?->readAsString());
